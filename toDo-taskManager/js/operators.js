8459716bc3fcd612b80485ffca731e9b33510bc7
inspherepraveen <insphere.praveen@gmail.com>
2025-09-08 18:15:12 +0530

    learning operators

---
+++ toDo-taskManager/js/operators.js
@@ -0,0 +1,17 @@
+// Task 1: Addition & Subtraction
+console.log(5+5);
+console.log("5"+5);
+console.log(5+"5");
+// console.log(Number("5"+"5"));
+console.log(Number("5")+Number("5"));
+console.log(+"5" + +"10");
+console.log(+"5" + -"10");
+
+
+document.getElementById("one").innerHTML="Ans: = " + (7 + 8);
+document.getElementById("two").innerHTML="Ans: = "+ 7+ "8";
+document.getElementById("three").innerHTML="Ans: = " + (Number("7")+ Number("8"));
+document.getElementById("three").innerHTML="Ans: = " +(+"7" + +"8");
+document.getElementById("four").innerHTML="Ans: = "+ (true + 5);
+document.getElementById("five").innerHTML="Ans: = " + 2 + 3 + 4;
+document.getElementById("five").innerHTML="Ans: = " + (Number("2")+Number("3")+Number("4"));
