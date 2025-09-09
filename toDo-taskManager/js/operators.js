db7acf772d6b54d55e837eff3e1f7b245a48444d
inspherepraveen <insphere.praveen@gmail.com>
2025-09-09 18:24:51 +0530

    addition

--- toDo-taskManager/js/operators.js
+++ toDo-taskManager/js/operators.js
@@ -13,5 +13,52 @@ document.getElementById("two").innerHTML="Ans: = "+ 7+ "8";
 document.getElementById("three").innerHTML="Ans: = " + (Number("7")+ Number("8"));
 document.getElementById("three").innerHTML="Ans: = " +(+"7" + +"8");
 document.getElementById("four").innerHTML="Ans: = "+ (true + 5);
-document.getElementById("five").innerHTML="Ans: = " + 2 + 3 + 4;
+document.getElementById("five").innerHTML="Ans: = " + (2 + 3 + 4);
 document.getElementById("five").innerHTML="Ans: = " + (Number("2")+Number("3")+Number("4"));
+
+const one = 7;
+const two = 8 ;
+const result = one + two;
+document.getElementById("one").innerHTML = "Ans: = "+ result;
+
+const primary = 7;
+const secondary = "8";
+const final = primary+secondary;
+document.getElementById("two").innerHTML="Ans: = " + final;
+
+const Number1 = "7";
+const Number2 = "8";
+const output = (Number(Number1)+Number(Number2))
+document.getElementById("three").innerHTML = "Ans: =" + output;
+
+const value1 = true;
+const value2 = 5;
+const merged = value1+value2;
+document.getElementById("four").innerHTML= "Ans: = " + merged;
+
+const op1 = "2";
+const op2 = "3";
+const op3 = "4";
+const value = (Number(op1)+Number(op2)+Number(op3));
+document.getElementById("five").innerHTML="Ans: = " + value;
+
+
+document.getElementById("six").innerHTML="Ans: = " + (10 - 3);
+document.getElementById("seven").innerHTML="Ans: = "+ ("10" - "3");
+document.getElementById("eight").innerHTML="Ans: = "+ (5 - true);
+document.getElementById("nine").innerHTML="Ans: = " + "hello" - 2;
+document.getElementById("ten").innerHTML="Ans: = " + ((20 -5) - 3);
+
+
+document.getElementById("multi1").innerHTML="Ans: =" + (5*4);
+document.getElementById("multi2").innerHTML="Ans: =" + ("5" * "4");
+document.getElementById("multi3").innerHTML="Ans: =" + (10*0);
+document.getElementById("multi4").innerHTML="Ans: =" + (-3*6);
+document.getElementById("multi5").innerHTML="Ans: =" + (2+3)*4;
+
+
+document.getElementById("exp1").innerHTML = "Ans: = " + (2 ** 3);
+document.getElementById("exp2").innerHTML="Ans: ="+ ;
+document.getElementById("exp3").innerHTML="Ans: ="+ ;
+document.getElementById("exp4").innerHTML="Ans: ="+ ;
+document.getElementById("exp5").innerHTML="Ans: ="+ ;
